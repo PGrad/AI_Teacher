c33391e1a5440565628bde84e7c34266433609e2
Paul Grad <paulgrad3rd@gmail.com>
2023-02-15 22:03:49 -0800

    Cool Animations!
    Added some cool animations that I kind of ripped
    off from here: web.dev/css-border-animations
    to make the story page more dynamic and
    interesting to look at.

--- src/Story.tsx
+++ src/Story.tsx
@@ -7,19 +7,7 @@ import { useParams } from 'react-router-dom';
 import Test from './Test';
 import { useLangContext } from './LocalizationProvider';
 import FunnyButton from './FunnyButton';
-
-function Loading() {
-  const [dots, setDots] = useState<number>(0);
-  useEffect(() => {
-    setTimeout(() => {
-      setDots((dots + 1) % 4);
-    }, 500);
-  }, [dots])
-  const dotStr: string = ".".repeat(dots)
-  return (
-    <h1 className='loading'>Loading{dotStr}</h1>
-  );
-}
+import BlankImg from "./blank.jpg";
 
 function makeReadable(text: string): string[] {
     return text.split("\n\n");
@@ -35,15 +23,19 @@ function getQuestions(sentence: string): string[] {
 function Story() {
   const { target, level } = useParams();
   const base = useLangContext();
+  let [storyHeading, setStoryHeading] = useState<string>("");
   let [storyText, setStoryText] = useState<string[]>([]);
   let [storyImg, setStoryImg] = useState<string>("");
   let [storyQuestions, setStoryQuestions] = useState<string[]>([]);
   let [showTest, setShowTest] = useState<boolean>(false);
 
   useEffect(() => {
-    Api.getStoryText(`write me a story in ${target} at a ${level} reading level and then make 4 questions in ${base} based on the story.`).then((data) => {
-      const text = makeReadable(data.data.choices[0].text!);
+    Api.getStoryText(`write me a story with a title in ${target} at a ${level} reading level and then make 4 questions in ${base} based on the story.`).then((data) => {
+      let text = makeReadable(data.data.choices[0].text!);
       const questions = getQuestions(text.splice(text.length - 1)[0]);
+      const title = text[1].split(":")[1];
+      setStoryHeading(title);
+      text = text.slice(2);
       text.splice(text.length - 1);
       setStoryText(text);
       setStoryQuestions(questions);
@@ -58,15 +50,25 @@ function Story() {
   };
 
   return (
-    storyImg ?
-    <div className='story'>
-      <ImageCard imgName={storyImg} />
-      <StoryText paragraphs={storyText} />
-      {showTest ?
-        <Test questions={storyQuestions} /> :
-        <FunnyButton onClick={onClick}>Take the Test</FunnyButton>
+    <div className={`story ${storyImg ? "paused" : ""}`}>
+      <section className='story-text'>
+        <h1 style={{ textAlign: "center" }}>{storyHeading}</h1>
+        <StoryText paragraphs={storyText} />
+      </section>
+      { storyImg ? (
+        <section className='story-test'>
+          {showTest ?
+            <Test questions={storyQuestions} /> :
+            <FunnyButton onClick={onClick}>Take the Test</FunnyButton>
+          }
+        </section>): ""
       }
-    </div> : <Loading />
+      <div className='img'>
+        <div className='img-card'>
+          <ImageCard imgName={storyImg ? storyImg : BlankImg} />
+        </div>
+      </div>
+    </div>
   );
 }
 

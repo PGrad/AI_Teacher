08975e78e5475a0727a47e4261198d38728f8d36
Paul Grad <paulgrad3rd@gmail.com>
2023-02-15 22:38:34 -0800

    Style changes
    If Houdini is not supported, use
    default values for background.
    Make fonts bigger and contrast better.

--- src/FunnyButton.tsx
+++ src/FunnyButton.tsx
@@ -7,6 +7,19 @@ interface FunnyButtonProps {
 
 export default function FunnyButton(props: React.PropsWithChildren<FunnyButtonProps>) {
     return (
-        <Button sx={{ fontFamily: "Rubik Bubbles" }} variant="contained" onClick={props.onClick}>{props.children}</Button>
+        <Button
+            sx={{
+                fontFamily: "Rubik Bubbles",
+                fontSize: "1.5em",
+                borderRadius: "1em",
+                padding: ".5em 1em",
+                backgroundColor: "teal",
+                color: "white"
+            }}
+            variant="contained"
+            onClick={props.onClick}
+        >
+            {props.children}
+        </Button>
     );
 }

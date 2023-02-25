878267e0bac731af93d141092690488f919a7227
Paul Grad <paulgrad3rd@gmail.com>
2023-02-25 15:41:45 -0800

    Using react-helmet to avoid linting html
    If something's wrong with the head tags,
    it's easier to spot them in React than
    in the index.html.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import * as Api from "./Api";
 import CreateStory from "./CreateStory";
 import { LocalizationProvider } from "./LocalizationProvider";
 import Story from "./Story";
+import { Helmet } from 'react-helmet';
 
 export default function App() {
     const prefersDarkMode: boolean = useMediaQuery('(prefers-color-scheme: dark)');
@@ -23,15 +24,39 @@ export default function App() {
     }, []);
 
     return (
-        <ThemeProvider theme={theme}>
-            <LocalizationProvider lang={navigator.language.split("-")[0]}>
-                <HashRouter basename='/'>
-                    <Routes>
-                        <Route path="/" element={<CreateStory />} />
-                        <Route path="/story/:target/:level" element={<Story />} />
-                    </Routes>
-                </HashRouter>
-            </LocalizationProvider>
-        </ThemeProvider>
+        <>
+            <Helmet>
+                <title>AI Teacher</title>
+                <meta
+                name="description"
+                content="Learn languages with AI!"
+                />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="AI Language Teacher" />
+                <meta property="og:description" content="Learn languages with AI!"/>
+                <meta property="og:url" content="https://pgrad.github.io/AI_Teacher/" />
+                <meta property="og:image" content="%PUBLIC_URL%/Thumbnail.png" />
+
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:domain" content="pgrad.github.io"/>
+                <meta property="twitter:url" content="https://pgrad.github.io/AI_Teacher/"/>
+                <meta name="twitter:title" content="AI Language Teacher"/>
+                <meta name="twitter:description" content="Learn languages with AI!"/>
+                <meta name="twitter:image" content="%PUBLIC_URL%/Thumbnail.png" />
+            </Helmet>
+            <ThemeProvider theme={theme}>
+                <LocalizationProvider lang={navigator.language.split("-")[0]}>
+                    <HashRouter basename='/'>
+                        <main>
+                            <Routes>
+                                <Route path="/" element={<CreateStory />} />
+                                <Route path="/story/:target/:level" element={<Story />} />
+                            </Routes>
+                        </main>
+                    </HashRouter>
+                </LocalizationProvider>
+            </ThemeProvider>
+        </>
     );
 }

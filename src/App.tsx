9d6f4a246fededa419847155e41e83f9cc686abd
Paul Grad <paulgrad3rd@gmail.com>
2023-02-25 18:43:16 -0800

    Remove react-helmet, not using it

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import * as Api from "./Api";
 import CreateStory from "./CreateStory";
 import { LocalizationProvider } from "./LocalizationProvider";
 import Story from "./Story";
-import { Helmet } from 'react-helmet';
 
 export default function App() {
     const prefersDarkMode: boolean = useMediaQuery('(prefers-color-scheme: dark)');
@@ -24,39 +23,17 @@ export default function App() {
     }, []);
 
     return (
-        <>
-            <Helmet>
-                <title>AI Teacher</title>
-                <meta
-                name="description"
-                content="Learn languages with AI!"
-                />
-
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="AI Language Teacher" />
-                <meta property="og:description" content="Learn languages with AI!"/>
-                <meta property="og:url" content="https://pgrad.github.io/AI_Teacher/" />
-                <meta property="og:image" content={`${process.env.PUBLIC_URL}/Thumbnail.png`} />
-
-                <meta name="twitter:card" content="summary_large_image"/>
-                <meta property="twitter:domain" content="pgrad.github.io"/>
-                <meta property="twitter:url" content="https://pgrad.github.io/AI_Teacher/"/>
-                <meta name="twitter:title" content="AI Language Teacher"/>
-                <meta name="twitter:description" content="Learn languages with AI!"/>
-                <meta name="twitter:image" content={`${process.env.PUBLIC_URL}/Thumbnail.png`} />
-            </Helmet>
-            <ThemeProvider theme={theme}>
-                <LocalizationProvider lang={navigator.language.split("-")[0]}>
-                    <HashRouter basename='/'>
-                        <main>
-                            <Routes>
-                                <Route path="/" element={<CreateStory />} />
-                                <Route path="/story/:target/:level" element={<Story />} />
-                            </Routes>
-                        </main>
-                    </HashRouter>
-                </LocalizationProvider>
-            </ThemeProvider>
-        </>
+        <ThemeProvider theme={theme}>
+            <LocalizationProvider lang={navigator.language.split("-")[0]}>
+                <HashRouter basename='/'>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<CreateStory />} />
+                            <Route path="/story/:target/:level" element={<Story />} />
+                        </Routes>
+                    </main>
+                </HashRouter>
+            </LocalizationProvider>
+        </ThemeProvider>
     );
 }
